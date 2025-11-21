f7291af60005e279c4043d36722c4efedadcdcc2
Matthew Pearson <matthewpearson@gmail.com>
2025-11-20 23:37:02 -0800

    Last round of changes for tonight

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Looptid Microsystems";
-export const SITE_DESCRIPTION = "Infrastructure consulting for genomics and global health. Modernizing mission-driven systems that can't afford downtime.";
+export const SITE_DESCRIPTION = "Infrastructure consulting for genomics and global health. Modernizing mission-driven systems that can’t afford downtime.";
