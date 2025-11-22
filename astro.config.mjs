73f2530da09759a2af31568b3032ccb597653dae
Matthew Pearson <matthewpearson@gmail.com>
2025-11-22 11:26:30 -0800

    Additional cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
+    imageService: "compile",  // just to silence a build warning
     platformProxy: {
       enabled: true,
     },
